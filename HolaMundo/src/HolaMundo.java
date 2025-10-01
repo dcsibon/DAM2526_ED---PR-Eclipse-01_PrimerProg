665355e8092648be927dc14430bd041325832094
Diego Cano Sibón <dcsibon@gmail.com>
2025-10-01 20:06:12 +0200

    Add files via upload

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,14 @@
+
+/**
+ * Class HolaMundo: primer programa en Java que imprime un mensaje por pantalla.
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("¡Hola, Eclipse!");
+	}
+
+}
